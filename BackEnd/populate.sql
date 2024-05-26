85393ea9a1cbbf851f2b495e8d0e0e011b4ca7d3
Agape <theaman249@gmail.com>
2024-05-26 03:08:22 +0200

    Initial data sql files

---
+++ BackEnd/populate.sql
@@ -0,0 +1,30 @@
+INSERT INTO modules VALUES 
+    ('COS151', 'introduction to computer science', 1, 1),
+    ('COS132', 'imperative programming', 1, 1),
+    ('COS110', 'program design: Introduction 110', 1, 2),
+    ('COS122', 'operating systems', 1, 2),
+    ('WTW115', 'discrete mathematics', 1, 2),
+    ('WTW114', 'calculus 114', 1, 1),
+    ('COS212', 'data structures and algorithms', 2, 1),
+    ('COS221', 'introduction to database design', 2, 1),
+    ('COS216', 'netcentric systems', 2, 1),
+    ('COS214', 'software modelling', 2, 2),
+    ('COS226', 'concurrent systems', 2, 2),
+    ('COS284', 'computer organisation and Architecture', 2, 2),
+    ('COS332', 'computer networks', 3, 1),
+    ('COS330', 'computer security', 3, 2),
+    ('COS301', 'software engineering', 3, 0),
+    ('COS333', 'programming languages', 3, 1),
+    ('COS341', 'compiler construction', 3, 2),
+    ('COS314', 'artificial intelligence', 3, 1),
+    ('COS326', 'database systems', 3, 2),
+    ('COS344', 'computer graphics', 3, 1);
+
+INSERT INTO students_modules VALUES
+    ('u18105883', 'COS330'),
+    ('u18105883', 'COS301'),
+    ('u18105883', 'COS332'),
+    ('u18105883', 'COS333'),
+    ('u18105883', 'COS326'),
+    ('u18105883', 'COS341');
+
